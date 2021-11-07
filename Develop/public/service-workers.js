22b25ad9489fc2dffe4e9e32c86dad4b29005324
megan.campbell.1988@gmail.com <megan.campbell.1988@gmail.com>
2021-11-07 11:20:16 -0600

    service worker.js

--- Develop/public/service-workers.js
+++ Develop/public/service-workers.js
@@ -1,20 +1,49 @@
+const { cache } = require("webpack");
 
+const APP_PREFIX = 'BankTransfer';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE =[
+const FILES_TO_CACHE = [
     "./index.html",
     "./css/style.css",
     "./js/index.js",
-    "./manifest.json", 
+    "./manifest.json",
     "./icons/icon-72x72.png",
     "./icons/icon-96x96.png",
     "./icons/icon-128x128.png",
     "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png", 
+    "./icons/icon-384x384.png",
     "./icons/icon-512x512.png"
-    
+
 ]
 
-self.addEventListener('install', function(e) {
-
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 });
+
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        cache.key().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cache.cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeepList.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return cache.delete(keyList[i])
+                    }
+                })
+            );
+        })
+    )
+})
